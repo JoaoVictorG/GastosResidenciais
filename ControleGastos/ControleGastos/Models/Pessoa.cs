3cbec73b00fa47b90a8973451659a939d9eb869b
JoaoVictorG <joaovictorgon36@gmail.com>
2022-10-29 17:27:09 -0300

    link nav e display name

--- ControleGastos/ControleGastos/Models/Pessoa.cs
+++ ControleGastos/ControleGastos/Models/Pessoa.cs
@@ -1,10 +1,15 @@
-﻿namespace ControleGastos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Models
 {
     public class Pessoa
     {
         public int Id { get; set; }
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [Display(Name = "Idade")]
         public int Idade { get; set; }
+        [Display(Name = "Data de Registro")]
         public DateTime DataRegistro { get; set; } = DateTime.Now.Date;
 
         public virtual List<Transacao>? Transacaos { get; set; }
--- ControleGastos/ControleGastos/Models/Transacao.cs
+++ ControleGastos/ControleGastos/Models/Transacao.cs
@@ -1,12 +1,17 @@
-﻿namespace ControleGastos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Models
 {
     public class Transacao
     {
         public int Id { get; set; }
         public int PessoaId { get; set; }
-        public Pessoa Pessoa { get; set; }
+        public Pessoa? Pessoa { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+        [Display(Name = "Tipo")]
         public string Tipo { get; set; }
+        [Display(Name = "Valor")]
         public decimal Valor { get; set; }
     }
 }
